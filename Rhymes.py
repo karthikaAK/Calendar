7f89a439c301eb6e6ba63989ba87020ac17aa0a9
karthikaAK <134278019+karthikaAK@users.noreply.github.com>
2023-07-06 15:34:49 +0530

    Second commit

---
+++ Rhymes.py
@@ -0,0 +1,3 @@
+print("Twinkle, twinkle, little star, \n\t\t How I Wonder what you are!")
+print("\t\t\t Up above the world so high, \n\t\t\t Like a diamond in the sky.")
+print("Twinkle,twinkle, little star, \n\t\t How I wonder what you are!")
